4ac4eef18e659991c935a1fece0940d0090d68d0
kishor <kishor@kishors-MacBook-Pro.local>
2018-03-16 06:22:40 +0530

    Completed

---
+++ WeatherApp/New Group/WeatherTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  WeatherTableViewCell.swift
+//  WeatherApp
+//
+//  Created by kishor on 3/16/18.
+//  Copyright © 2018 shreyas. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
